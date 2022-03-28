f964fec2bf17093e47cdf48023343997ecdfa743
Germán jesus V-P <gviola@glerp.net.co>
2022-03-28 16:41:17 -0500

    Agregar archivos de proyecto.

---
+++ BlazorChat/Server/Hubs/ChatHub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChat.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private static Dictionary<string, string> usuarios = new Dictionary<string,string>();
+
+        //metodo para conectarse
+        public override async Task OnConnectedAsync()
+        {
+            string username = Context.GetHttpContext().Request.Query["username"];
+            usuarios.Add(Context.ConnectionId, username);
+            await AddMessageToChat(string.Empty, $"**{username} a ingresado al chat epicamente**");
+            await base.OnConnectedAsync();
+        }
+        //metodo para desconectarse
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            //evaluamos si el usuario esta conectado
+            string username = usuarios.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            await AddMessageToChat(string.Empty, $"**{username} Abandonó el chat**");
+
+        }
+
+        //metodo que añade los mensaje
+        public async Task AddMessageToChat( string user, string message)
+        {
+            await Clients.All.SendAsync("Mensaje Recibido",user, message);
+        }
+
+
+
+    }
+}
---
+++ BlazorChat/Server/Program.cs
@@ -0,0 +1,50 @@
+using BlazorChat.Server.Hubs;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container. 
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+//agregamos el servicio de signalR
+builder.Services.AddSignalR();
+//se establece el tipo de mimica o interaccion
+builder.Services.AddResponseCompression(
+    options => options.MimeTypes = ResponseCompressionDefaults 
+    .MimeTypes
+    .Concat
+    (new[] { "application/octet-stream" })
+    
+    );
+
+var app = builder.Build();
+app.UseResponseCompression();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+//agregamos lo que seria el endpoint
+app.MapHub<ChatHub>("/chathub");
+app.MapFallbackToFile("index.html");
+
+app.Run();
